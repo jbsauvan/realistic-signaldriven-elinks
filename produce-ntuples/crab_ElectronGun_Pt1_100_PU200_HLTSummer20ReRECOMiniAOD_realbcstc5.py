e109fb37db4a22f377f8df4f820f05b6f181294e
Jean-Baptiste Sauvan <jean-baptiste.sauvan@cern.ch>
2022-08-10 12:04:17 +0200

    add electron gun crab configs

---
+++ produce-ntuples/crab_ElectronGun_Pt1_100_PU200_HLTSummer20ReRECOMiniAOD_realbcstc5.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append('.')
+from CRABClient.UserUtilities import config
+import local
+
+config = config()
+
+config.section_("General")
+config.General.requestName = 'ElectronGun_Pt1_100_PU200_HLTSummer20ReRECOMiniAOD_{}_realbcstc5'.format(local.prod_name)
+config.General.workArea = 'jobs'
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'produce_ntuple_realbcstc5_xyseed_reduced_genmatch_v11_cfg.py'
+config.JobType.maxMemoryMB = 2500
+
+config.section_("Data")
+config.Data.inputDataset = '/DoubleElectron_FlatPt-1To100/Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_111X_mcRun4_realistic_T15_v1-v2/GEN-SIM-DIGI-RAW-MINIAOD'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 10
+config.Data.outLFNDirBase = local.outLFNDirBase
+config.Data.publication = False
+config.Data.outputDatasetTag = config.General.requestName
+
+config.section_("Site")
+config.Site.storageSite = local.storageSite
